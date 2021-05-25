004d0b528e42246f89c17ec8e60fb7cb518b4f81
AuroraMyftaraj40 <myftarajsidrit@gmail.com>
2021-05-25 19:29:16 -0400

    Initial commit

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,11 @@
+public class BranchingPractice {
+    public static void main(String[] args) {
+        //create a new project
+        // to enable version control
+        //adding .gitignore with .idea, target folder and iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
+
+    }
+}
